2ec952bba02bb8ce02bd7104e17dc7324a858e01
Matthias Klein <matthias@klein.pw>
2016-04-16 12:56:36 +0200

    use extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -8,15 +8,19 @@ module.exports = {
     library: 'reactHearthstone',
     path: path.join(__dirname, 'build'),
     filename: 'react-hearthstone.js',
-    publicPath: 'http://matkl.github.io/react-hearthstone/build/'
+    publicPath: '/react-hearthstone/build/'
   },
   externals: {
     'react': 'React'
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', query: { presets: ['react'] } }
+      { test: /\.js$/, loader: 'babel-loader', query: { presets: ['react'] } },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
   devtool: 'source-map'
 };
