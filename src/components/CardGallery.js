1248ca4d1ba9773117287619c686c25f87cf8a10
Matthias Klein <matthias@klein.pw>
2016-04-16 12:58:02 +0200

    add CardGallery component

--- src/components/CardGallery.js
+++ src/components/CardGallery.js
@@ -1,1 +1,16 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import CardGalleryItem from './CardGalleryItem';
+
+export default class CardGallery extends Component {
+  render() {
+    return (
+      <ul className="hs-CardGallery">
+        {this.props.cards.map(card => <CardGalleryItem key={card.id} {...card} />)}
+      </ul>
+    );
+  }
+}
+
+CardGallery.propTypes = {
+  cards: PropTypes.array
+};
