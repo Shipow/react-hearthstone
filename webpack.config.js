e110124f0cda015f8a12fb0396797355b3d266fb
Matthias Klein <matthias@klein.pw>
2016-04-16 13:45:18 +0200

    use postcss with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
@@ -16,9 +17,10 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', query: { presets: ['react'] } },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader') }
     ]
   },
+  postcss: [autoprefixer()],
   plugins: [
     new ExtractTextPlugin('style.css')
   ],
