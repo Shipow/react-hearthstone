eaf20204fdfa1146f3e2be10cfe251c31f6d5817
Matthias Klein <matthias@klein.pw>
2016-04-23 06:35:42 +0200

    add tests

--- src/components/DeckBuilder.js
+++ src/components/DeckBuilder.js
@@ -12,17 +12,34 @@ export default class DeckBuilder extends Component {
       deck: []
     };
     this.addCardToDeck = this.addCardToDeck.bind(this);
+    this.removeCardFromDeck = this.removeCardFromDeck.bind(this);
   }
   componentDidMount() {
     $.get('https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json', function(cards) {
       cards.forEach(function(card) {
         card.imageUrl = 'http://wow.zamimg.com/images/hearthstone/cards/enus/medium/' + card.id + '.png';
       });
+
       this.setState({ cards: cards });
     }.bind(this));
   }
   addCardToDeck(card) {
     this.setState({ deck: this.state.deck.concat(card) });
+  }
+  removeCardFromDeck(card) {
+    const cardId = card.id;
+    let found = false;
+
+    // remove first occurence of the card
+    const deck = this.state.deck.filter(function(card, index) {
+      if (!found && card.id == cardId) {
+        found = true;
+        return false;
+      }
+      return true;
+    });
+
+    this.setState({ deck: deck });
   }
   render() {
     return (
@@ -31,7 +48,7 @@ export default class DeckBuilder extends Component {
           <CardGallery cards={this.state.cards.slice(0,30)} onClick={this.addCardToDeck} />
         </div>
         <div className="hs-DeckBuilder__deck">
-          <CardList cards={this.state.deck} />
+          <CardList cards={this.state.deck} onClick={this.removeCardFromDeck} />
         </div>
       </div>
     );
--- test/DeckBuilder.js
+++ test/DeckBuilder.js
@@ -1,15 +1,40 @@
 import { expect } from 'chai';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 import DeckBuilder from '../src/components/DeckBuilder';
+import collectibleCards from './hearthstonejson/cards.collectible.json';
 
 describe('DeckBuilder', function() {
-  const renderer = ReactTestUtils.createRenderer();
-
   it('renders', function() {
-    renderer.render(<DeckBuilder />)
-    const result = renderer.getRenderOutput();
-    expect(result.props.className).to.equal('hs-DeckBuilder');
+    const wrapper = shallow(<DeckBuilder />);
+    expect(wrapper.hasClass('hs-DeckBuilder')).to.be.true;
+  });
+
+  it('contains a CardGallery and a CardList', function() {
+    const wrapper = shallow(<DeckBuilder />);
+    expect(wrapper.find('CardGallery')).to.have.length.of(1);
+    expect(wrapper.find('CardList')).to.have.length.of(1);
+  });
+
+  it('displays a collection', function() {
+    const wrapper = shallow(<DeckBuilder />);
+    const collection = collectibleCards.slice(0, 2);
+    wrapper.setState({ cards: collection });
+    expect(wrapper.find('CardGallery').props().cards).to.have.length(collection.length);
+  });
+
+  it('adds cards to the deck', function() {
+    const wrapper = shallow(<DeckBuilder />);
+    expect(wrapper.find('CardList').props().cards).to.have.length(0);
+    wrapper.find('CardGallery').simulate('click');
+    expect(wrapper.find('CardList').props().cards).to.have.length(1);
+  });
+
+  it('removes cards from the deck', function() {
+    const wrapper = shallow(<DeckBuilder />);
+    const deck = collectibleCards.slice(0, 2);
+    wrapper.setState({ deck: deck });
+    expect(wrapper.find('CardList').props().cards).to.have.length(deck.length);
+    wrapper.find('CardList').simulate('click', deck[0]);
+    expect(wrapper.find('CardList').props().cards).to.have.length(deck.length - 1);
   });
 });
