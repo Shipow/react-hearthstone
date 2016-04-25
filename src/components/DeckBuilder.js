519815f8a39214d74c1f74187fa2c6c9f5f6285e
Matthias Klein <matthias@klein.pw>
2016-04-25 17:14:52 +0200

    add class filter

--- src/components/DeckBuilder.js
+++ src/components/DeckBuilder.js
@@ -4,15 +4,23 @@ import CardGallery from './CardGallery';
 import CardList from './CardList';
 import './DeckBuilder.css';
 
+function capitalize(str) {
+  if (!str) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
 export default class DeckBuilder extends Component {
   constructor(props) {
     super(props);
     this.state = {
       cards: [],
-      deck: []
+      deck: [],
+      showClassCards: true
     };
     this.addCardToDeck = this.addCardToDeck.bind(this);
     this.removeCardFromDeck = this.removeCardFromDeck.bind(this);
+    this.showClassCards = this.showClassCards.bind(this);
+    this.showNeutralCards = this.showNeutralCards.bind(this);
   }
   componentDidMount() {
     $.get('https://api.hearthstonejson.com/v1/latest/enUS/cards.collectible.json', function(cards) {
@@ -41,11 +49,38 @@ export default class DeckBuilder extends Component {
 
     this.setState({ deck: deck });
   }
+  showClassCards() {
+    this.setState({ showClassCards: true });
+  }
+  showNeutralCards() {
+    this.setState({ showClassCards: false });
+  }
+  getVisibleCards() {
+    let cards = this.state.cards;
+
+    cards = cards.filter(card => card.type == 'MINION' || card.type == 'SPELL' || card.type == 'WEAPON');
+
+    if (this.state.showClassCards) {
+      cards = cards.filter(card => this.props.playerClass == card.playerClass);
+    } else {
+      cards = cards.filter(card => card.playerClass == null);
+    }
+
+    return cards;
+  }
   render() {
+    const visibleCards = this.getVisibleCards();
+    
     return (
       <div className="hs-DeckBuilder">
+        <div className="hs-DeckBuilder__header">
+          <div className="hs-DeckBuilder__pages">
+            <a onClick={this.showClassCards}>{capitalize(this.props.playerClass.toLowerCase())}</a>
+            <a onClick={this.showNeutralCards}>Neutral</a>
+          </div>
+        </div>
         <div className="hs-DeckBuilder__collection">
-          <CardGallery cards={this.state.cards.slice(0,30)} onClick={this.addCardToDeck} />
+          <CardGallery cards={visibleCards} onClick={this.addCardToDeck} />
         </div>
         <div className="hs-DeckBuilder__deck">
           <CardList cards={this.state.deck} onClick={this.removeCardFromDeck} />
@@ -54,3 +89,7 @@ export default class DeckBuilder extends Component {
     );
   }
 }
+
+DeckBuilder.propTypes = {
+  playerClass: PropTypes.string.isRequired
+};
